9d6b618f877df019d10ea7c4295f4136cf20e7c5
Rohit Shelot <dev.rohitshelot@gmail.com>
2025-06-11 14:45:24 +0530

    Again Changes Made.

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 const express = require('express')
-const app = express();
+const app = express()
 const dotenv = require('dotenv').config()
 const mainRouter = require('./router/mainRouter.js')
-const DB_conn = require('./Db_conn.js');
-const cookieParser = require('cookie-parser');
+const DB_conn = require('./Db_conn.js')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const userRouter = require('./router/userProfile.js')
 
-const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
-app.use('/api/auth',mainRouter);
-app.use('/',userRouter)
+app.use('/api/auth', mainRouter)
+app.use('/', userRouter)
 
 DB_conn().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+    console.log('Connected to database')
 }).catch((error) => {
-    console.error('Error in Server:', error);
-});
-module.exports = app;
+    console.error('Database connection error:', error)
+})
+
+module.exports = app // Do not use app.listen() for Vercel deployment
