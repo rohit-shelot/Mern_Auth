0c603ff81b598a57bba9edcd9fb17092bcc62e94
Rohit Shelot <dev.rohitshelot@gmail.com>
2025-06-11 14:17:49 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const userRouter = require('./router/userProfile.js')
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
@@ -15,11 +15,11 @@ app.use(cookieParser())
 app.use('/api/auth',mainRouter);
 app.use('/',userRouter)
 
-DB_conn().then(()=>{
-
-
-}
-).catch(()=>{
-    console.log(`Error in Server.`)
-})
+DB_conn().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((error) => {
+    console.error('Error in Server:', error);
+});
 module.exports = app;
